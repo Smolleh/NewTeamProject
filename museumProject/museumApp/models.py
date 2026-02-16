38485abff4ed9945fab5097a5845a86565938fe7
Jimi <jimim10@outlook.com>
2026-02-16 12:10:44 +0000

    users

--- museumProject/museumApp/models.py
+++ museumProject/museumApp/models.py
@@ -31,17 +31,31 @@ class AiSystemDescription(models.Model):
         managed = True
         db_table = 'AI_system_description'
 
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+    first_name = models.CharField(max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
 
 
+class BookMarks(models.Model):
+    pk = models.CompositePrimaryKey('userId', 'exhibitId')
+    userId = models.ForeignKey(AuthUser, models.CASCADE, db_column='userId', blank=True, null=False)   
+    exhibitId = models.ForeignKey(Exhibit, models.CASCADE, db_column='exhibitId', blank=True, null=False)   
 
-# class BookMarks(models.Model):
-#     pk = models.CompositePrimaryKey('userId', 'exhibitId')
-#     userId = models.ForeignKey(Users, models.CASCADE, db_column='userId', blank=True, null=False)   
-#     exhibitId = models.ForeignKey(Exhibit, models.CASCADE, db_column='exhibitId', blank=True, null=False)   
-
-#     class Meta:
-#         managed = False
-#         db_table = 'book_marks'
+    class Meta:
+        managed = False
+        db_table = 'book_marks'
 
 
 
